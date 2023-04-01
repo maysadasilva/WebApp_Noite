e8c5d2f46baf9fa9381948d989dff3bb640e8d54
Ferneko <fcgraciano@gmail.com>
2023-03-31 22:08:13 -0300

    Controller CLiente, Views e link no menu

---
+++ WebApp_Noite/Controllers/ClienteController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Noite.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
