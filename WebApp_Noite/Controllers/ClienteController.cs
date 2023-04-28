be90bdc245f5ae9f4b7c97010b81fa3e3e81a96c
Ferneko <fcgraciano@gmail.com>
2023-04-28 20:15:52 -0300

    Cadastro de clientes e lista de clientes estatica

--- WebApp_Noite/Controllers/ClienteController.cs
+++ WebApp_Noite/Controllers/ClienteController.cs
@@ -1,17 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_Noite.Models;
 
 namespace WebApp_Noite.Controllers
 {
     public class ClienteController : Controller
     {
+
+        public static List<ClientesModel> db = new List<ClientesModel>();
+
         public IActionResult Lista()
         {
-            return View();
+            return View( db );
         }
 
         public IActionResult Cadastrar()
         {
-            return View();
+            ClientesModel model = new ClientesModel();
+            return View( model );
+        }
+
+        [HttpPost]
+        public IActionResult SalvarDados(ClientesModel cliente)
+        {
+            if(cliente.Id == 0)
+            {
+                Random rand = new Random();
+
+                cliente.Id = rand.Next(1,9999 );
+                db.Add(cliente);
+            }
+
+            return RedirectToAction("Lista");
         }
     }
 }
---
+++ WebApp_Noite/Models/ClientesModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp_Noite.Models
+{
+    public class ClientesModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Telefone { get; set; }
+
+
+
+    }
+}
