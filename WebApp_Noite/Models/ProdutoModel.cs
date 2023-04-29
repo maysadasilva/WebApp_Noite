e307ac585e5f5ae36e25936c4c2357c45de042bc
MAYSA SILVA <maysa.silva4@fatec.sp.gov.br>
2023-04-28 21:49:59 -0300

    criando model de produto

---
+++ WebApp_Noite/Models/ProdutoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp_Noite.Models
+{
+    public class ProdutoModel
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public string QtdEstoque { get; set; }
+
+    }
+}
